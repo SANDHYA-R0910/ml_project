d96cc5aaf0161852f93f96f46073ecbf188d42bf
SANDHYA-R0910 <sandhyarsanju@gmail.com>
2023-07-04 12:20:18 +0530

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,13 @@
 import sys   # sys module provides functions and variables which are used to manipulate different parts of the Python Runtime Environment
 import logging
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     filename = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        filename,exc_tb.tb_lineno,str(error))
+    filename,exc_tb.tb_lineno,str(error))
     return error_message
 
 class CustomException(Exception):
