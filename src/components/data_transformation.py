2f10eb7eac5851f71276a2485cec72fc9bae6566
SANDHYA-R0910 <sandhyarsanju@gmail.com>
2023-07-19 09:58:52 +0530

    prediction pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -75,7 +75,7 @@ class DataTransformation:
                 
             preprocessing_obj = self.get_data_transformation_object()
             target_column_name  = "math score"
-            numerical_columns = ['writing score','reading_score']
+            numerical_columns = ['reading score','writing score']
                 
             input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df = train_df[target_column_name]
